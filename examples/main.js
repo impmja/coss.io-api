4a5c8ee1ffeae850cea19e3f877f2c5d5ac99230
impmja@gmx.de <impmja@gmx.de>
2018-02-27 16:11:13 +0100

    Updated the examples a bit. Also fixed some javascript bugs.. within the example.

--- examples/main.js
+++ examples/main.js
@@ -1,4 +1,4 @@
-const CossIOLib = require('./../lib/coss-io');
+const CossIOLib = require('./../lib');
 
 const main = async () => {
   try {
@@ -30,8 +30,8 @@ const main = async () => {
     // try {
     //   const placedOrder = await cossIO.placeOrder({
     //     symbol: 'coss-eth',
-    //     side: CossIOOrderSide.BUY,
-    //     type: CossIOOrderType.LIMIT,
+    //     side: CossIOLib.CossIOOrderSide.BUY,
+    //     type: CossIOLib.CossIOOrderType.LIMIT,
     //     price: 0.0005,
     //     amount: 10,
     //     session,
@@ -58,45 +58,45 @@ const main = async () => {
     //   console.error('Failed to request order history', error);
     // }
 
-    // // try {
-    // //   const depth = await cossIO.requestDepth({ symbol: 'coss-eth' });
-    // //   console.log('Depth:');
-    // //   console.log('First Bid', depth.bids[0]);
-    // //   console.log('First Ask', depth.asks[0]);
-    // //   console.log('---------------------------------');
-    // // } catch (error) {
-    // //   console.error('Failed to request depth', error);
-    // // }
+    // try {
+    //   const depth = await cossIO.requestDepth({ symbol: 'coss-eth' });
+    //   console.log('Depth:');
+    //   console.log('First Bid', depth.bids[0]);
+    //   console.log('First Ask', depth.asks[0]);
+    //   console.log('---------------------------------');
+    // } catch (error) {
+    //   console.error('Failed to request depth', error);
+    // }
 
-    // // try {
-    // //   const tickers: CossIOTickerList = await cossIO.requestTickers();
-    // //   console.log('All Tickers:');
-    // //   for (const pair of tickers) {
-    // //     console.log(`- '${pair.tradingPair.id}'`);
-    // //   }
-    // //   console.log('---------------------------------');
-    // // } catch (error) {
-    // //   console.error('Failed to request ticker list', error);
-    // // }
+    // try {
+    //   const tickers = await cossIO.requestTickers();
+    //   console.log('All Tickers:');
+    //   for (const pair of tickers) {
+    //     console.log(`- '${pair.tradingPair.id}'`);
+    //   }
+    //   console.log('---------------------------------');
+    // } catch (error) {
+    //   console.error('Failed to request ticker list', error);
+    // }
 
-    // // const intervalTicker = async () => {
-    // //   try {
-    // //     const tickerCossETH = await cossIO.requestTicker({ symbol: 'coss-eth' });
-    // //     console.log('Ticker: COSS/ETH');
-    // //     console.log(`Current Price: '${tickerCossETH.price}'`);
-    // //     console.log('---------------------------------');
+    const intervalTicker = async () => {
+      try {
+        const tickerCossETH = await cossIO.requestTicker({ symbol: 'coss-eth' });
+        console.log('Ticker: COSS/ETH');
+        console.log(`Current Price: '${tickerCossETH.price}'`);
+        console.log('---------------------------------');
 
-    // //     const tickerCossBTC = await cossIO.requestTicker({ symbol: 'coss-btc' });
-    // //     console.log('Ticker: COSS/BTC');
-    // //     console.log(`Current Price: '${tickerCossBTC.price}'`);
-    // //     console.log('---------------------------------');
-    // //   } catch (error) {
-    // //     console.error('Failed to request ticker', error);
-    // //   }
-    // // };
+        const tickerCossBTC = await cossIO.requestTicker({ symbol: 'coss-btc' });
+        console.log('Ticker: COSS/BTC');
+        console.log(`Current Price: '${tickerCossBTC.price}'`);
+        console.log('---------------------------------');
+      } catch (error) {
+        console.error('Failed to request ticker', error);
+      }
+    };
 
-    // intervalTicker();
-    // setInterval(intervalTicker, 10000);
+    intervalTicker();
+    setInterval(intervalTicker, 10000);
   } catch (error) {
     console.error('Something bad happend :/', error);
   }
--- examples/main.ts
+++ examples/main.ts
@@ -58,42 +58,42 @@ const main = async () => {
     //   console.error('Failed to request order history', error);
     // }
 
-    // // try {
-    // //   const depth = await cossIO.requestDepth({ symbol: 'coss-eth' });
-    // //   console.log('Depth:');
-    // //   console.log('First Bid', depth.bids[0]);
-    // //   console.log('First Ask', depth.asks[0]);
-    // //   console.log('---------------------------------');
-    // // } catch (error) {
-    // //   console.error('Failed to request depth', error);
-    // // }
+    // try {
+    //   const depth = await cossIO.requestDepth({ symbol: 'coss-eth' });
+    //   console.log('Depth:');
+    //   console.log('First Bid', depth.bids[0]);
+    //   console.log('First Ask', depth.asks[0]);
+    //   console.log('---------------------------------');
+    // } catch (error) {
+    //   console.error('Failed to request depth', error);
+    // }
 
-    // // try {
-    // //   const tickers: CossIOTickerList = await cossIO.requestTickers();
-    // //   console.log('All Tickers:');
-    // //   for (const pair of tickers) {
-    // //     console.log(`- '${pair.tradingPair.id}'`);
-    // //   }
-    // //   console.log('---------------------------------');
-    // // } catch (error) {
-    // //   console.error('Failed to request ticker list', error);
-    // // }
+    // try {
+    //   const tickers: CossIOTickerList = await cossIO.requestTickers();
+    //   console.log('All Tickers:');
+    //   for (const pair of tickers) {
+    //     console.log(`- '${pair.tradingPair.id}'`);
+    //   }
+    //   console.log('---------------------------------');
+    // } catch (error) {
+    //   console.error('Failed to request ticker list', error);
+    // }
 
-    // // const intervalTicker = async () => {
-    // //   try {
-    // //     const tickerCossETH = await cossIO.requestTicker({ symbol: 'coss-eth' });
-    // //     console.log('Ticker: COSS/ETH');
-    // //     console.log(`Current Price: '${tickerCossETH.price}'`);
-    // //     console.log('---------------------------------');
+    // const intervalTicker = async () => {
+    //   try {
+    //     const tickerCossETH = await cossIO.requestTicker({ symbol: 'coss-eth' });
+    //     console.log('Ticker: COSS/ETH');
+    //     console.log(`Current Price: '${tickerCossETH.price}'`);
+    //     console.log('---------------------------------');
 
-    // //     const tickerCossBTC = await cossIO.requestTicker({ symbol: 'coss-btc' });
-    // //     console.log('Ticker: COSS/BTC');
-    // //     console.log(`Current Price: '${tickerCossBTC.price}'`);
-    // //     console.log('---------------------------------');
-    // //   } catch (error) {
-    // //     console.error('Failed to request ticker', error);
-    // //   }
-    // // };
+    //     const tickerCossBTC = await cossIO.requestTicker({ symbol: 'coss-btc' });
+    //     console.log('Ticker: COSS/BTC');
+    //     console.log(`Current Price: '${tickerCossBTC.price}'`);
+    //     console.log('---------------------------------');
+    //   } catch (error) {
+    //     console.error('Failed to request ticker', error);
+    //   }
+    // };
 
     // intervalTicker();
     // setInterval(intervalTicker, 10000);
